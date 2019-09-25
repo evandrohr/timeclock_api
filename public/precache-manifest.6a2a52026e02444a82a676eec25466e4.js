ef96dba2de59c9ca840525f13e43131104d51c17
Evandro <evandrohr@eopt.com.br>
2019-09-25 07:25:41 -0300

    Deploying Reactjs app

---
+++ public/precache-manifest.6a2a52026e02444a82a676eec25466e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dd4eab560a32b6c313dba5e4175d556",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6dac7bb10ea4b13e8f82",
+    "url": "/static/css/main.37900330.chunk.css"
+  },
+  {
+    "revision": "a56b33659c3b39c6aba4",
+    "url": "/static/js/2.3008dbe6.chunk.js"
+  },
+  {
+    "revision": "6dac7bb10ea4b13e8f82",
+    "url": "/static/js/main.80a54c41.chunk.js"
+  },
+  {
+    "revision": "5abf6c41cfda3cec9f51",
+    "url": "/static/js/runtime-main.224cd3d7.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
